ad4f6439bc65ebffc489ead3bc41ec780e5a631d
Almaz <minnexanov.a@mail.ru>
2023-02-17 15:16:52 +0300

    seminar 7 Task 49

--- seminar7task49/Program.cs
+++ seminar7task49/Program.cs
@@ -28,9 +28,9 @@ int[,] GetArray(int m, int n, int min, int max)
 
 int[,] NumbersSquar (int[,] matrix)
 {
-    for(int i = 1; i < matrix.GetLength(0); i ++)
+    for(int i = 0; i < matrix.GetLength(0); i ++)
     {
-        for(int j = 1; j < matrix.GetLength(1); j ++)
+        for(int j = 0; j < matrix.GetLength(1); j ++)
         {
             if((i%2==0)&&(j%2==0)) matrix [i,j] = (matrix [i,j])*(matrix [i,j]);
         }
