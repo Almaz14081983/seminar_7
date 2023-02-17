e9c63d52a83cef1513e09eb338a9fc1fa9a8ee79
Almaz <minnexanov.a@mail.ru>
2023-02-17 13:29:53 +0300

    seminar 7 Task 46

---
+++ seminar7task46/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 46, Задайте двумерный массив размером m*n, 
+// заполненный случайными числами.
+// m= 3, n=4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1.
+
+Console.Write("Введите колличество строк массива  ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите колличество строк массива  ");
+int columens = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray( rows, columens, 0, 10);
+
+//int[,] array =  GetArray( 4, 5, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int [m,n];
+    for(int i = 0; i < m; i ++)
+    {
+        for(int j = 0; j < n; j ++)
+        {
+            result[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i ++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
