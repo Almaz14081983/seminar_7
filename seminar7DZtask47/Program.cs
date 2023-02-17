7d6658c93a83ce019dc98ab51032fb95bf35ffad
Almaz <minnexanov.a@mail.ru>
2023-02-17 16:42:40 +0300

    seminar 7 dz Task 47

--- seminar7DZtask47/Program.cs
+++ seminar7DZtask47/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите колличество строк массива  ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите колличество столбцов массива  ");
+int columens = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное значение в массиве ");
+int min = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальное значение в массиве ");
+int max = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray( rows, columens, min, max);
+
+//int[,] array =  GetArray( 4, 5, 0, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int min, int max)
+{
+    double[,] result = new double [m,n];
+    for(int i = 0; i < m; i ++)
+    {
+        for(int j = 0; j < n; j ++)
+        {
+            result[i,j] = Math.Round(new Random().NextDouble() + new Random().Next(min, max), 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i ++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
